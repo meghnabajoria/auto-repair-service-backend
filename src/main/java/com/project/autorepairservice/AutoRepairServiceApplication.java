28bea36126c0247a6a4757279e0bff0786774cd8
meghnabajoria <meghnabajoria8@gmail.com>
2024-02-28 20:24:00 -0800

    initialise

---
+++ src/main/java/com/project/autorepairservice/AutoRepairServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.autorepairservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRepairServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoRepairServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/autorepairservice/AutoRepairServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autorepairservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRepairServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
