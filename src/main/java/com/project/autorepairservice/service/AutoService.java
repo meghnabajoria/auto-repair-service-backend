61d115cc6c0efc254c94a19597d80d117724248f
meghnabajoria <meghnabajoria8@gmail.com>
2024-03-09 15:18:10 -0800

    code for DGS netflix

--- src/main/java/com/project/autorepairservice/service/AutoService.java
+++ src/main/java/com/project/autorepairservice/service/AutoService.java
@@ -1,6 +1,5 @@
 package com.project.autorepairservice.service;
 
-import com.project.autorepairservice.Models.ServiceModel;
 import com.project.autorepairservice.dto.request.ServiceRequest;
 import com.project.autorepairservice.dto.response.ServiceResponse;
 
