ff1e535774ffab23652c05cfd5f45203be23fa13
MdShafeeqU <mohamedshafeeq.usman@gmail.com>
2024-03-09 18:03:18 -0800

    Revert "code for graphql"
    This reverts commit 3388bb8f46c4f56c9873e830c41daaadf5daef1e.

--- src/main/java/com/project/autorepairservice/AutoRepairServiceApplication.java
+++ src/main/java/com/project/autorepairservice/AutoRepairServiceApplication.java
@@ -1,12 +1,7 @@
 package com.project.autorepairservice;
 
-import com.project.autorepairservice.resolvers.ServiceModelQueryResolver;
-import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -15,11 +10,7 @@ public class AutoRepairServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoRepairServiceApplication.class, args);
-	}
-
-	@Bean
-	public ServiceModelQueryResolver query() {
-		return new ServiceModelQueryResolver();
+		System.out.println("hiiii");
 	}
 
 }
