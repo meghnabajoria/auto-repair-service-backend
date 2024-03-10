0ef6662b707eff75fe73d7cb1fe7e9319ac90380
MdShafeeqU <mohamedshafeeq.usman@gmail.com>
2024-03-09 17:23:06 -0800

    added addservice mutation

--- src/main/java/com/project/autorepairservice/dto/request/ServiceRequest.java
+++ src/main/java/com/project/autorepairservice/dto/request/ServiceRequest.java
@@ -1,17 +1,21 @@
 package com.project.autorepairservice.dto.request;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServiceRequest {
     private String name;
     private String desc;
-    private String ServiceType;
+    private String serviceType;
     private String imgUrl;
     private List<Integer> offeredAtStores;
 }
