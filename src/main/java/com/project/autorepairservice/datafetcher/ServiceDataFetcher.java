6a344b1643e62dc9368fd39cf0d4c9a817e51771
MdShafeeqU <mohamedshafeeq.usman@gmail.com>
2024-03-10 00:46:58 -0800

    Test commit

--- src/main/java/com/project/autorepairservice/datafetcher/ServiceDataFetcher.java
+++ src/main/java/com/project/autorepairservice/datafetcher/ServiceDataFetcher.java
@@ -2,9 +2,11 @@ package com.project.autorepairservice.datafetcher;
 
 
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 import com.project.autorepairservice.Models.ServiceModel;
+import com.project.autorepairservice.dto.request.ServiceRequest;
 import com.project.autorepairservice.repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -24,4 +26,20 @@ public class ServiceDataFetcher {
     public ServiceModel getServiceById(@InputArgument String id) {
         return serviceModelRepository.findById(id).orElse(null);
     }
+
+    @DgsMutation(field = "addService")
+    public ServiceModel addService(@InputArgument ServiceRequest input) {
+        // Create a new ServiceModel object from the input
+        ServiceModel newService = ServiceModel.builder()
+                .name(input.getName())
+                .desc(input.getDesc())
+                .serviceType(input.getServiceType())
+                .imgUrl(input.getImgUrl())
+                .offeredAtStores(input.getOfferedAtStores())
+                .build();
+
+        // Save the new service to the repository
+        return serviceModelRepository.save(newService);
+    }
 }
+
