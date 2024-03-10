2c906a093e9a4a3729d9b190aff6a1f2e8363e1b
meghnabajoria <meghnabajoria8@gmail.com>
2024-03-09 17:41:47 -0800

    added query for store, created helper for model to response conversion

--- src/main/java/com/project/autorepairservice/datafetcher/storeDataFetcher.java
+++ src/main/java/com/project/autorepairservice/datafetcher/storeDataFetcher.java
@@ -2,14 +2,12 @@ package com.project.autorepairservice.datafetcher;
 
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
-import com.project.autorepairservice.Models.ContactDetails;
 import com.project.autorepairservice.Models.StoreModel;
 import com.project.autorepairservice.dto.response.StoreResponse;
 import com.project.autorepairservice.repository.StoreRepository;
 import com.project.autorepairservice.service.helpers.ModelToResponseHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
